fdee974dea4aec0b81916b3f15d65f8a5e8ff7bb
Harish Mohan Raj <harish@airt.ai>
2023-11-14 19:45:35 +0530

    Remove duplicate css class

--- src/client/ChatPage.jsx
+++ src/client/ChatPage.jsx
@@ -40,7 +40,7 @@ const ChatsList = ({ chats }) => {
 const Loader = () => {
     return (
         <div className="absolute top-[38%] left-[45%] -translate-y-2/4 -translate-x-2/4">
-            <div className="w-12 h-12 border-4 border-white rounded-full rounded-lg animate-spin border-t-captn-light-blue border-t-4"></div>
+            <div className="w-12 h-12 border-4 border-white rounded-full animate-spin border-t-captn-light-blue border-t-4"></div>
         </div>
     )
 }
