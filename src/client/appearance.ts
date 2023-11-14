dacf0d6a8d0cd456975c557c65ad7ddb77eb2685
Harish Mohan Raj <harish@airt.ai>
2023-11-14 20:26:01 +0530

    WIP

--- src/client/appearance.ts
+++ src/client/appearance.ts
@@ -2,8 +2,8 @@ import type { CustomizationOptions } from '@wasp/auth/forms/types'
 
 export const appearance: CustomizationOptions['appearance'] = {
   colors: {
-    brand: '#0da37f', 
-    brandAccent: '#066d55',
-    submitButtonText: 'white',
+    brand: '#f7ff00', 
+    brandAccent: '#f7ff00',
+    submitButtonText: 'black',
   },
 }
