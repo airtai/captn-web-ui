020726f5630d95d36e02aea57d88b1ec8aec0605
Harish Mohan Raj <harish@airt.ai>
2023-11-17 07:07:29 +0530

    Restrict access to other user conversations

--- src/client/components/Loader.tsx
+++ src/client/components/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
-    return (
-        <div className="absolute top-[38%] left-[45%] -translate-y-2/4 -translate-x-2/4">
-            <div className="w-12 h-12 border-4 border-white rounded-full animate-spin border-t-captn-light-blue border-t-4"></div>
-        </div>
-    )
+  return (
+    <div className="absolute top-[38%] left-[45%] -translate-y-2/4 -translate-x-2/4">
+      <div className="w-12 h-12 border-4 border-white rounded-full animate-spin border-t-captn-light-blue border-t-4"></div>
+    </div>
+  );
 }
