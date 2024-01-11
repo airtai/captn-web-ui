06a70bb1b6d8667e2d1b758adf91e937f4855691
Harish Mohan Raj <harish@airt.ai>
2024-01-11 19:40:36 +0530

    Hot fix (#93)

--- src/client/PricingPage.tsx
+++ src/client/PricingPage.tsx
@@ -64,7 +64,8 @@ export default function PricingPage() {
                 </h3>
                 <div className="mt-4 flex items-baseline gap-x-2">
                   <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    {price.priceMonthly || price.price}
+                    {/* {price.priceMonthly || price.price} */}
+                    {price.priceMonthly}
                   </span>
                   {/* {price.priceMonthly && (
                     <span className="text-base font-semibold leading-7 text-gray-600">
@@ -93,11 +94,12 @@ export default function PricingPage() {
               <button
                 onClick={clickHandler}
                 aria-describedby={price.id}
-                disabled={price.disabled}
-                className={`${
-                  price.disabled &&
-                  "disabled:opacity-25 disabled:cursor-not-allowed"
-                } mt-8 block rounded-md bg-yellow-400 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-black shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600`}
+                // disabled={price.disabled}
+                // className={`${
+                //   price.disabled &&
+                //   "disabled:opacity-25 disabled:cursor-not-allowed"
+                // } mt-8 block rounded-md bg-yellow-400 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-black shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600`}
+                className={`mt-8 block rounded-md bg-yellow-400 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-black shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600`}
               >
                 {isLoading ? "Loading..." : "Buy Now"}
               </button>
