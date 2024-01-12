58de0172e0cfb00c4be1a777bafa2aa54744c244
Harish Mohan Raj <harish@airt.ai>
2024-01-12 15:40:35 +0530

    Hide email login (#99)

--- src/client/auth/LoginPage.tsx
+++ src/client/auth/LoginPage.tsx
@@ -1,10 +1,10 @@
-import { useHistory } from 'react-router-dom';
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { LoginForm } from '@wasp/auth/forms/Login';
-import { AuthWrapper } from './authWrapper';
-import useAuth from '@wasp/auth/useAuth';
-import { appearance } from '../appearance'
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { LoginForm } from "@wasp/auth/forms/Login";
+import { AuthWrapper } from "./authWrapper";
+import useAuth from "@wasp/auth/useAuth";
+import { appearance } from "../appearance";
 
 export default function Login() {
   const history = useHistory();
@@ -13,29 +13,31 @@ export default function Login() {
 
   useEffect(() => {
     if (user) {
-      history.push('/');
+      history.push("/");
     }
   }, [user, history]);
 
   return (
     <AuthWrapper>
-      <LoginForm appearance={appearance}/>
-      <br />
-      <span className='text-sm font-medium text-gray-900'>
-        Don't have an account yet?{' '}
-        <Link to='/signup' className='underline'>
-          go to signup
-        </Link>
-        .
-      </span>
-      <br />
-      <span className='text-sm font-medium text-gray-900'>
-        Forgot your password?{' '}
-        <Link to='/request-password-reset' className='underline'>
-          reset it
-        </Link>
-        .
-      </span>
+      <div className="login-form-wrapper">
+        <LoginForm appearance={appearance} />
+        <br />
+        <span className="text-sm font-medium text-gray-900">
+          Don't have an account yet?{" "}
+          <Link to="/signup" className="underline">
+            go to signup
+          </Link>
+          .
+        </span>
+        <br />
+        <span className="text-sm font-medium text-gray-900">
+          Forgot your password?{" "}
+          <Link to="/request-password-reset" className="underline">
+            reset it
+          </Link>
+          .
+        </span>
+      </div>
     </AuthWrapper>
   );
 }
