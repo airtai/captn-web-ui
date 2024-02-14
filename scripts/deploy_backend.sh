d2231ff415127cdf79bb113b97ce471ac1ac4f37
Kumaran Rajendhiran <kumaran@airt.ai>
2024-02-14 12:09:05 +0530

    Optimize deploy (#160)

--- scripts/deploy_backend.sh
+++ scripts/deploy_backend.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 
-if test -z "$TAG"
-then
-	echo "ERROR: TAG variable must be defined, exiting"
-	exit -1
-fi
+# Check if variables are defined
+check_variable() {
+    if [ -z "${!1}" ]; then
+        echo "ERROR: $1 variable must be defined, exiting"
+        exit -1
+    fi
+}
 
-if test -z "$GITHUB_USERNAME"
-then
-	echo "ERROR: GITHUB_USERNAME variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$GITHUB_PASSWORD"
-then
-	echo "ERROR: GITHUB_PASSWORD variable must be defined, exiting"
-	exit -1
-fi
+# Check for required variables
+check_variable "TAG"
+check_variable "GITHUB_USERNAME"
+check_variable "GITHUB_PASSWORD"
+check_variable "BACKEND_DOMAIN"
+check_variable "PORT"
+check_variable "DATABASE_URL"
+check_variable "WASP_WEB_CLIENT_URL"
+check_variable "JWT_SECRET"
+check_variable "ADS_SERVER_URL"
+check_variable "STRIPE_KEY"
+check_variable "PRO_SUBSCRIPTION_PRICE_ID"
+check_variable "STRIPE_WEBHOOK_SECRET"
 
 
 if [ ! -f key.pem ]; then
@@ -26,71 +30,26 @@ if [ ! -f key.pem ]; then
 fi
 
 
-if test -z "$BACKEND_DOMAIN"
-then
-	echo "ERROR: BACKEND_DOMAIN variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$PORT"
-then
-	echo "ERROR: PORT variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$DATABASE_URL"
-then
-	echo "ERROR: DATABASE_URL variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$WASP_WEB_CLIENT_URL"
-then
-	echo "ERROR: WASP_WEB_CLIENT_URL variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$JWT_SECRET"
-then
-	echo "ERROR: JWT_SECRET variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$ADS_SERVER_URL"
-then
-	echo "ERROR: ADS_SERVER_URL variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$STRIPE_KEY"
-then
-	echo "ERROR: STRIPE_KEY variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$PRO_SUBSCRIPTION_PRICE_ID"
-then
-	echo "ERROR: PRO_SUBSCRIPTION_PRICE_ID variable must be defined, exiting"
-	exit -1
-fi
-
-if test -z "$STRIPE_WEBHOOK_SECRET"
-then
-	echo "ERROR: STRIPE_WEBHOOK_SECRET variable must be defined, exiting"
-	exit -1
-fi
+ssh_command="ssh -o StrictHostKeyChecking=no -i key.pem azureuser@\"$BACKEND_DOMAIN\""
 
 echo "INFO: stopping already running docker container"
-ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$BACKEND_DOMAIN" "docker stop wasp-backend || echo 'No containers available to stop'"
-ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$BACKEND_DOMAIN" "docker container prune -f || echo 'No stopped containers to delete'"
+$ssh_command "docker stop wasp-backend || echo 'No containers available to stop'"
+$ssh_command "docker container prune -f || echo 'No stopped containers to delete'"
 
 echo "INFO: pulling docker image"
-ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$BACKEND_DOMAIN" "echo $GITHUB_PASSWORD | docker login -u '$GITHUB_USERNAME' --password-stdin '$REGISTRY'"
-ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$BACKEND_DOMAIN" "docker pull ghcr.io/$GITHUB_REPOSITORY:'$TAG'"
+$ssh_command "echo $GITHUB_PASSWORD | docker login -u '$GITHUB_USERNAME' --password-stdin '$REGISTRY'"
+$ssh_command "docker pull ghcr.io/$GITHUB_REPOSITORY:'$TAG'"
 sleep 10
 
 echo "Deleting old image"
-ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$BACKEND_DOMAIN" "docker system prune -f || echo 'No images to delete'"
+$ssh_command "docker system prune -f || echo 'No images to delete'"
 
 echo "INFO: starting docker container"
-ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$BACKEND_DOMAIN" "docker run --name wasp-backend -p $PORT:$PORT -e PORT='$PORT' -e DATABASE_URL='$DATABASE_URL' -e WASP_WEB_CLIENT_URL='$WASP_WEB_CLIENT_URL' -e JWT_SECRET='$JWT_SECRET' -e GOOGLE_CLIENT_ID='$GOOGLE_CLIENT_ID' -e GOOGLE_CLIENT_SECRET='$GOOGLE_CLIENT_SECRET' -e AZURE_OPENAI_API_KEY='$AZURE_OPENAI_API_KEY' -e ADS_SERVER_URL='$ADS_SERVER_URL' -e STRIPE_KEY='$STRIPE_KEY' -e PRO_SUBSCRIPTION_PRICE_ID='$PRO_SUBSCRIPTION_PRICE_ID' -e STRIPE_WEBHOOK_SECRET='$STRIPE_WEBHOOK_SECRET' -e ADMIN_EMAILS='$ADMIN_EMAILS' -d ghcr.io/$GITHUB_REPOSITORY:$TAG"
+$ssh_command "docker run --name wasp-backend -p $PORT:$PORT -e PORT='$PORT' \
+    -e DATABASE_URL='$DATABASE_URL' -e WASP_WEB_CLIENT_URL='$WASP_WEB_CLIENT_URL' \
+	-e JWT_SECRET='$JWT_SECRET' -e GOOGLE_CLIENT_ID='$GOOGLE_CLIENT_ID' \
+	-e GOOGLE_CLIENT_SECRET='$GOOGLE_CLIENT_SECRET' \
+	-e AZURE_OPENAI_API_KEY='$AZURE_OPENAI_API_KEY' -e ADS_SERVER_URL='$ADS_SERVER_URL' \
+	-e STRIPE_KEY='$STRIPE_KEY' -e PRO_SUBSCRIPTION_PRICE_ID='$PRO_SUBSCRIPTION_PRICE_ID' \
+	-e STRIPE_WEBHOOK_SECRET='$STRIPE_WEBHOOK_SECRET' -e ADMIN_EMAILS='$ADMIN_EMAILS' \
+	-d ghcr.io/$GITHUB_REPOSITORY:$TAG"
