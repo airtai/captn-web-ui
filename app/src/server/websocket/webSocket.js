ea475bfada63c29c73c76fe76722e971afc2fa9e
Harish Mohan Raj <harish@airt.ai>
2024-03-05 23:21:51 +0530

    Fix socket protocol

--- app/src/server/websocket/webSocket.js
+++ app/src/server/websocket/webSocket.js
@@ -4,7 +4,10 @@ import WebSocket from 'ws';
 export const ADS_SERVER_URL =
   process.env.ADS_SERVER_URL || 'http://127.0.0.1:9000';
 
-const WS_URL = `ws://${ADS_SERVER_URL.split('//')[1].split(':')[0]}:8080`;
+const protocol = ADS_SERVER_URL === 'http://127.0.0.1:9000' ? 'ws' : 'wss';
+const WS_URL = `${protocol}://${
+  ADS_SERVER_URL.split('//')[1].split(':')[0]
+}:8080`;
 
 async function checkTeamStatus(context, socket, chat_id) {
   let json;
