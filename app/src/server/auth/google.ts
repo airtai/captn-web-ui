a6e7004a4e8abe24a6cf5e62a5ef9c13dbe51def
Harish Mohan Raj <harish@airt.ai>
2024-03-15 11:13:21 +0530

    WIP: Migrate to new auth

--- app/src/server/auth/google.ts
+++ app/src/server/auth/google.ts
@@ -1,40 +1,19 @@
-// TODO: Removed `GetUserFieldsFn` from "@wasp/types" import because it is deprecated and has no clear alternative. Please check migration instructions in Wasp docs on how to manually migrate the code that was using it.
-// TODO: Removed `generateAvailableUsername` from "@wasp/core/auth" import because it is deprecated and has no clear alternative. Please check migration instructions in Wasp docs on how to manually migrate the code that was using it.
-// export const getUserFields: GetUserFieldsFn = async (
-//   _context: any,
-//   args: any
-// ) => {
-//   const email = args.profile.emails[0].value;
-//   const username = await generateAvailableUsername(
-//     args.profile.displayName.split(' '),
-//     { separator: '.' }
-//   );
-//   const adminEmails = process.env.ADMIN_EMAILS?.split(',') || [];
-//   const isAdmin = adminEmails.includes(email);
-//   return { email, username, isAdmin };
-// };
-
-// export function config() {
-//   const clientID = process.env.GOOGLE_CLIENT_ID;
-//   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-//   return {
-//     clientID, // look up from env or elsewhere,
-//     clientSecret, // look up from env or elsewhere,
-//     scope: ['profile', 'email'], // must include at least 'profile' for Google
-//   };
-// }
-
-import { defineUserSignupFields } from 'wasp/server/auth'
+import { defineUserSignupFields } from 'wasp/server/auth';
+import { generateAvailableUsername } from './authHelper';
 
 export const userSignupFields = defineUserSignupFields({
-  username: (data: any) => data.profile.displayName,
+  username: async (data: any) => {
+    return await generateAvailableUsername(data.profile.displayName, {
+      separator: '.',
+    });
+  },
   email: (data: any) => data.profile.emails[0].value,
   isAdmin: async (data: any) => {
     const email = data.profile.emails[0].value;
     const adminEmails = process.env.ADMIN_EMAILS?.split(',') || [];
     return adminEmails.includes(email);
   },
-})
+});
 
 export function getConfig() {
   const clientID = process.env.GOOGLE_CLIENT_ID;
@@ -43,5 +22,5 @@ export function getConfig() {
     clientID, // look up from env or elsewhere
     clientSecret, // look up from env or elsewhere
     scope: ['profile', 'email'],
-  }
+  };
 }
