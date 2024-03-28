7680677452708e3190e5bdebadf51f2a7f093504
Harish Mohan Raj <harish@airt.ai>
2024-03-28 16:54:26 +0530

    Fix tests

--- app/src/client/tests/App.test.tsx
+++ app/src/client/tests/App.test.tsx
@@ -1,4 +1,4 @@
-import { renderInContext } from "wasp/client/test";
+import { renderInContext } from 'wasp/client/test';
 import { test } from 'vitest';
 import { screen } from '@testing-library/react';
 
@@ -7,7 +7,7 @@ import App from '../App';
 test('renders App component', async () => {
   renderInContext(<App children={<div>Test</div>} />);
 
-  await screen.findByText('Test');
+  await screen.findByText('Please wait a moment while we log you in.');
 
   screen.debug();
 });
