63b3cf1d7231d07e6d369a95785802e6cd60f79d
Kumaran Rajendhiran <kumaran@airt.ai>
2024-04-01 16:31:02 +0530

    Fix ws_ping output to log command (#281)

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Start websocket ping
-python3 /app/.wasp/build/server/ws_ping.py > /app/.wasp/build/server/ws_ping.log &
+python3 /app/.wasp/build/server/ws_ping.py > /app/.wasp/build/server/ws_ping.log 2>&1 &
 # tail -f /app/.wasp/build/server/ws_ping.log &
 
 # Start node server
