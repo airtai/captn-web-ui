c1a52acd903109d660fc2a276fb8b2cf9b39a6ba
Harish Mohan Raj <harish@airt.ai>
2024-04-02 12:31:49 +0530

    Show the error message on top of the screen above the nav bar (#282)

--- app/src/client/components/ServerNotRechableComponent.tsx
+++ app/src/client/components/ServerNotRechableComponent.tsx
@@ -1,27 +1,15 @@
-import { ReactNode } from 'react';
-
 import AnimatedCharacterLoader from './AnimatedCharacterLoader';
 
-export default function ServerNotRechableComponent({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function ServerNotRechableComponent() {
   return (
-    <div>
-      <div
-        className='sticky top-0 z-999 flex w-full justify-center bg-captn-cta-red'
-        style={{ position: 'absolute', zIndex: '10000', top: '0', left: '0' }}
-      >
-        <AnimatedCharacterLoader
-          loadingMessage={
-            "Oops! Something went wrong. Our server is currently unavailable. Please do not refresh your browser. We're trying to reconnect..."
-          }
-          showLogo={false}
-          bgColor='bg-captn-cta-red'
-        />
-      </div>
-      <div>{children}</div>
+    <div className='server-error-component sticky top-0 z-999 flex w-full justify-center bg-captn-rose-quartz'>
+      <AnimatedCharacterLoader
+        loadingMessage={
+          "Oops! Something went wrong. Our server is currently unavailable. Please do not refresh your browser. We're trying to reconnect..."
+        }
+        showLogo={false}
+        bgColor='bg-captn-rose-quartz'
+      />
     </div>
   );
 }
